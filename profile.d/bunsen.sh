6128d22aa164c22881f028d6b5a984d619f25878
johnraff <github@rafflesnagoya.com>
2018-03-19 17:29:11 +0900

    Use Xsession.d/22bunsen-user-setup to trigger setup script

--- profile.d/bunsen.sh
+++
@@ -1,9 +0,0 @@
-# /etc/profile.d/bunsen.sh
-# BunsenLabs message
-
-if [ ! -f "${HOME}/.config/bunsen/bl-setup" ] && [ "$TERM" = "linux" ]; then
-    echo "
-There seem to be no configuration files for the BunsenLabs desktop.
-Please run 'bl-user-setup' if you'd like to generate them.
-"
-fi
